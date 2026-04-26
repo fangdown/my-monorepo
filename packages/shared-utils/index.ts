33272e6fefea2447ee572e33f92354ec8e7ef239
fangdown <409951398@qq.com>
2026-04-26 20:41:09 +0800

    feat(shared-utils): 添加 calculateSum 工具函数

--- packages/shared-utils/index.ts
+++ packages/shared-utils/index.ts
@@ -1,3 +1,7 @@
 export const greet = (name: string): string => {
-    return `Hello, ${name}!`;
-  };
+  return `Hello, ${name}!`;
+};
+
+export const calculateSum = (a: number, b: number): number => {
+  return a + b;
+};
